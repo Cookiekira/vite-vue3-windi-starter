a5b78ef9a0d27d0274a77ebb1095294bcd5895d7
Cookiekira <1308132372@qq.com>
2021-09-11 10:46:16 +0800

    Add a fun transforem by Windi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld.vue";
 </script>
 
 <template>
-<img alt="Vue logo" src="../assets/logo.png" class="inline" ><HelloWorld msg="Hello Vue 3 + TypeScript + Vite + WindiCSS"></HelloWorld>
+  <img
+    alt="Vue logo"
+    src="../assets/logo.png"
+    class="
+      relative
+      left-3.5rem
+      inline
+      transform
+      ease-linear
+      duration-4000
+      -rotate-1800
+      hover:(opacity-10
+      ease-linear
+      duration-4000
+      translate-x-35rem
+      rotate-1800
+      )
+    "
+  />
+  <img
+    class="
+      relative
+      right-6.2rem
+      -z-1
+      inline
+      mx-auto
+      max-w-full
+      w-auto
+      max-h-120px
+      block
+      select-none
+    "
+    src="../assets/logo.svg"
+    alt="Windi CSS logo"
+  />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + WindiCSS"></HelloWorld>
 </template>
 
 <script lang="ts">
@@ -14,7 +49,7 @@ import HelloWorld from '../components/HelloWorld.vue'
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
   },
 };
 </script>
